182bb58f33085c1002c6adfc78480f89085fe8b9
Aleksi Svaridze <aleksisvaridze@gmail.com>
2018-10-18 21:35:17 +0400

    ver_3

---
+++ precache-manifest.e709a3054bd3b5dd4fae0b7ece7492a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ce6192a152dbfdba98de1935602e0f2",
+    "url": "/static/media/me.6ce6192a.jpg"
+  },
+  {
+    "revision": "f75834b9d0b08997b024a769419263cb",
+    "url": "/static/media/Design.f75834b9.mp4"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "38ab0857afb44c65c154",
+    "url": "/static/js/main.38ab0857.chunk.js"
+  },
+  {
+    "revision": "c6472c041f98c5e23486",
+    "url": "/static/js/1.c6472c04.chunk.js"
+  },
+  {
+    "revision": "38ab0857afb44c65c154",
+    "url": "/static/css/main.01d483e0.chunk.css"
+  },
+  {
+    "revision": "cdef2f6459dd51b6a9ed51d50c9598b9",
+    "url": "/index.html"
+  }
+];
